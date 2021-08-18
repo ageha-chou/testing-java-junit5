bf07c84b1ab21abbc167ac7e444aed3d2c2306af
ageha-chou <agehachou2602@gmail.com>
2021-08-18 23:05:34 +0700

    return values from Mockito

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,6 +21,24 @@ class SpecialitySDJpaServiceTest {
 
     @InjectMocks //Mockito creates an instance of this and inject it
     private SpecialitySDJpaService service;
+
+    @Test
+    void findById() {
+        //create new Speciality object that our mock will return
+        Speciality speciality = new Speciality();
+
+        //when specialtyRepository findById() is called
+        //-> return an Optional of that Speciality object that we created on line 27
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        //make an actual call
+        Speciality foundSpecialty = service.findById(1L);
+
+        assertThat(foundSpecialty).isNotNull();
+
+        //make sure findById() only call once
+        verify(specialtyRepository).findById(1L);
+    }
 
     @Test
     void deleteById() {
