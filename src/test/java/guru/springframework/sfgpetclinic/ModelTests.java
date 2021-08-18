759c89cd798c1ce42aaf20297297c606d3e3af70
ageha-chou <agehachou2602@gmail.com>
2021-08-18 19:37:24 +0700

    parameterize with @ValueSource

--- src/test/java/guru/springframework/sfgpetclinic/ModelTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ModelTests.java
@@ -3,6 +3,10 @@ package guru.springframework.sfgpetclinic;
 import org.junit.jupiter.api.*;
 
 @Tag("models")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ModelTests {
-
+    @BeforeEach
+    default void setUp(TestInfo testInfo) {
+        System.out.println("Running Test - " + testInfo.getDisplayName());
+    }
 }
